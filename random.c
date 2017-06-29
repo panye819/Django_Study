94b7afcdccc16a1ec229c54b20be5d2ca8a3d5f1
panye819 <panye819@gmail.com>
2017-06-29 16:30:08 +0800

    Initialize random number generator

--- random.c
+++ random.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int random_int(int max)
 {
- return rand() % max;
+return rand() % max;
 }
- int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <number>\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-  int max = atoi(argv[1]);
-  int result = random_int(max);
-  printf("%d\n", result);
-  return EXIT_SUCCESS;
+if (argc != 2) {
+fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+return EXIT_FAILURE;
+}
+int max = atoi(argv[1]);
+srand(time(NULL));
+int result = random_int(max);
+printf("%d\n", result);
+return EXIT_SUCCESS;
 }
